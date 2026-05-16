96d8cb72ecb16e37aafd3170ac1da0c7c12aa231
Douglas Carmichael <dcarmich@dcarmichael.net>
2026-05-16 05:29:48 -0400

    Expand LPD to Levage & Portes Dauphine in the menu copyright
    LPD is the French manufacturer 'Levage & Portes Dauphine' (Lifting &
    Doors Dauphine). The DIAGNOSE menu copyright now spells out the
    company name -- same line in both EN and FR since 'Levage & Portes
    Dauphine' is a proper noun and doesn't translate.

--- Sources/ElevatorSystem/Localization/Strings.swift
+++ Sources/ElevatorSystem/Localization/Strings.swift
@@ -118,8 +118,8 @@ enum Strings {
                                     "Étage   Test                              Mesure         Statut")
         add("diag.menu.title",      "Diagnostic Test Selection",
                                     "Sélection des Tests de Diagnostic")
-        add("diag.menu.copyright",  "Copyright (c) 1985, 2026 LPD Systems, Inc.   All rights reserved.",
-                                    "Copyright (c) 1985, 2026 LPD Systèmes, Inc.   Tous droits réservés.")
+        add("diag.menu.copyright",  "Copyright (c) 1985, 2026 Levage & Portes Dauphine S.A.   All rights reserved.",
+                                    "Copyright (c) 1985, 2026 Levage & Portes Dauphine S.A.   Tous droits réservés.")
         add("diag.menu.nav",        "UP / DOWN to navigate    ENTER to run    Ctrl/Y to exit",
                                     "HAUT / BAS pour naviguer    ENTRÉE pour exécuter    Ctrl/Y pour quitter")
 
